d73a4dff67896e4d37023b2850e4fba2c71d08d5
Logan Coates <coates.logan1125@gmail.com>
2020-10-09 19:26:06 -0600

    added questions to java

---
+++ script.js
@@ -0,0 +1,18 @@
+var questionAnswers = {
+
+    question1: {
+        question: "How many innings are in a game?",
+        answers: ["10", "4", "9",],
+        correctAnswer: "9"
+    },
+    question2: {
+        question: "baseball is america's _______?",
+        answers: ["Past-time", "favorite sport", "most boring sport"],
+        correctAnswer: "Past-time"
+    },
+    question3: {
+        question: "What is baseball's championship called",
+        answers: ["Super bowl", "World series", "Finals"],
+        correctAnswer: "World series"
+    }
+};
